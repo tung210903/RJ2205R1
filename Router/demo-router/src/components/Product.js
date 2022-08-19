bf6542827a5e83787b610ea3e092a1441895579f
tung210903 <tungphamthanh2109@gmail.com>
2022-08-20 02:15:22 +0700

    Router

---
+++ Router/demo-router/src/components/Product.js
@@ -0,0 +1,11 @@
+import { useLocation } from "react-router-dom";
+
+function Product() {
+    const { state } = useLocation();
+    return (
+        <div>
+            <h3>Id selected {state.categoryId} </h3>
+        </div>
+    );
+}
+export default Product;
